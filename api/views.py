c2f5363ad9073c29c235bff494d6962f0f039c35
Bobby Eshleman <eshleman@pdx.edu>
2017-02-17 09:38:35 -0800

    Pass trail detail view test

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from api.models import Trail
+from api.serializers import TrailSerializer
 
-# Create your views here.
+class TrailViewSet(viewsets.ModelViewSet):
+    """
+    A simple viewset for viewing Trails.
+    """
+    queryset = Trail.objects.all()
+    serializer_class = TrailSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
