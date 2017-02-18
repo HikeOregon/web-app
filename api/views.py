de9b1054912eede3926cc5a3d2e173381379ceb0
Bobby Eshleman <eshleman@pdx.edu>
2017-02-18 13:58:01 -0800

    Integrated API swagger

--- api/views.py
+++ api/views.py
@@ -16,44 +16,6 @@ from api.serializers import TrailSerializer
 
 
 class TrailViewSet(viewsets.ModelViewSet):
-    """
-
-
-
-    This is the API endpoint for lists of trails and individual trail details.
-    You can use the **Filters** button in the top-right to use the search feature.
-
-    ## A Single Trail
-
-    Get individual trails by using their `id`.  For example, to get the trail
-    with `id=1`, use:
-
-        /api/trails/1
-
-
-    ## Name Search
-
-    To search for trails by name use the `search` parameter.  For example,
-    searching for a trail with the word *Lake* in the name:
-
-        /api/trails/?search=Lake
-
-    ## Query Parameters
-
-    The only supported parameters:  `difficulty`, `restroom`, `length` 
-
-
-    To get all trails with a difficulty of 4:
-
-        /api/trails/?difficulty=4
-
-    To get all trails with a difficulty of 4 and that have a restroom:
-
-        /api/trails/?difficulty=4&restroom=true
-
-
-    """
-
     queryset = Trail.objects.all()
     serializer_class = TrailSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -62,11 +24,6 @@ class TrailViewSet(viewsets.ModelViewSet):
     filter_fields = ('difficulty', 'restroom', 'length',)
 
     def retrieve(self, request, pk=None):
-        """Returns a list Response containing only one result.
-
-        The return format is the same as the list view.  This is done for API uniformity.
-
-        """
         queryset = Trail.objects.all()
         trail = get_object_or_404(queryset, pk=pk)
         # To keep the retrieve and list API's uniform, we pass in the single trail as a list
