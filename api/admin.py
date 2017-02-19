000b781b840637cbb56050fe26e674ebc0f17530
Bobby Eshleman <eshleman@pdx.edu>
2017-02-19 11:15:42 -0800

    Customized AdminSite

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from api.models import Trail
 
-@admin.register(Trail)
+
+class HikeOregonAdminSite(AdminSite):
+    site_header = 'Hike Oregon administration'
+
+
 class TrailAdmin(admin.ModelAdmin):
     pass
+
+admin_site = HikeOregonAdminSite(name='HikeOregon')
+admin_site.register(Trail, TrailAdmin)
